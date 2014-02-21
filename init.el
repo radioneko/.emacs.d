f75c8fc502dec25349c64b20952ffa19d861c469
kawaii.neko <kawaii.neko@pochta.ru>
2014-02-21 23:46:34 +0400

    remove speedbar as I dont use it

--- init.el
+++ init.el
@@ -141,33 +141,3 @@
   (align-regexp start end "\\(\\s-+\\)\\/[*/]" 1 1 nil))
 
 (global-set-key (kbd "C-c a") 'my-align-region)
-
-;; speedbar hackz
-(require 'speedbar)
-(defconst my-speedbar-buffer-name " SPEEDBAR")
-
-(defun my-speedbar-no-separate-frame ()
-  (interactive)
-  (when (not (buffer-live-p speedbar-buffer))
-    (setq speedbar-buffer (get-buffer-create my-speedbar-buffer-name)
-	  speedbar-frame (selected-frame)
-	  dframe-attached-frame (selected-frame)
-	  speedbar-select-frame-method 'attached
-	  speedbar-verbosity-level 0
-	  speedbar-last-selected-file nil)
-    (set-buffer speedbar-buffer)
-    (speedbar-mode)
-    (speedbar-reconfigure-keymaps)
-    (speedbar-update-contents)
-    (speedbar-set-timer 1)
-    (add-hook 'kill-buffer-hook
-	      (lambda () (when (eq (current-buffer) speedbar-buffer)
-			   (setq speedbar-frame nil
-				 dframe-attached-frame nil
-				 speedbar-buffer nil)
-			   (speedbar-set-timer nil))) t t))
-  (set-window-buffer (selected-window)
-		     (get-buffer my-speedbar-buffer-name)))
-
-
-
