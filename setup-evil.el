0fbe6284dfb592416e8bdfdc96ef0ca1768b554d
kawaii.neko <kawaii.neko@pochta.ru>
2014-02-26 02:45:36 +0400

    evil: yasnippet and ace-jump keybindings

--- setup-evil.el
+++ setup-evil.el
@@ -1,6 +1,12 @@
 (require 'evil)
 
 (define-key evil-normal-state-map [insert] 'evil-insert)
+(define-key evil-insert-state-map [C-return] 'yas/expand)
+;; ace-jump
+(define-key evil-normal-state-map (kbd "SPC") 'evil-ace-jump-char-mode)
+(define-key evil-normal-state-map (kbd "S-SPC") 'evil-ace-jump-char-to-mode)
+(define-key evil-normal-state-map (kbd "C-SPC") 'evil-ace-jump-word-mode)
+(define-key evil-normal-state-map (kbd "C-S-SPC") 'evil-ace-jump-line-mode)
 (evil-mode 1)
 
 (provide 'setup-evil)
